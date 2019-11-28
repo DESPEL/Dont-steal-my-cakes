8e6d3ad84a196156f822c5f62d4ffee2b14b239d
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2019-11-28 10:48:02 -0800

    Update MainMenu.cpp

--- JuegoClase/Classes/menus/MainMenu.cpp
+++ JuegoClase/Classes/menus/MainMenu.cpp
@@ -110,7 +110,7 @@ void MainMenu::initFunctions(std::vector<std::function <void(cocos2d::Ref*)>> fu
 	functions.push_back(CC_CALLBACK_0(MainMenu::actionButton9, this));
 }
 
-void MainMenu::actionButton1() { Director::getInstance()->pushScene(TransitionCrossFade::create(1, DebugScene::createScene())); }
+void MainMenu::actionButton1() { Director::getInstance()->pushScene(TransitionCrossFade::create(1, MenuIns::createScene())); }
 void MainMenu::actionButton2() { Director::getInstance()->end(); }
 void MainMenu::actionButton3() { Director::getInstance()->pushScene(TransitionCrossFade::create(1, MiniGameScene::createScene())); }
 void MainMenu::actionButton4() {  /*TODO*/ }
