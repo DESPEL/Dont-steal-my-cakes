29f8fb27f1d5927cf2ed81c3114f66d9fdd0caa4
Hector Osuna <55816095+FanGoH@users.noreply.github.com>
2019-11-28 19:26:06 -0800

    Update menor no cambia nada

--- JuegoClase/Classes/menus/OptionsMenu.h
+++ JuegoClase/Classes/menus/OptionsMenu.h
@@ -1,5 +1,32 @@
 #pragma once
-class OptionsMenu
-{
+
+#include "MotherMenu.h"
+#include "ui/CocosGUI.h"
+
+class OptionsMenu : public MotherMenu {
+private:
+	std::ostringstream _ostr;
+	cocos2d::ui::Slider* _effectsVolume;
+	cocos2d::ui::Slider* _backGroundVolume;
+	cocos2d::ui::Slider* dificulty;
+	cocos2d::Label* _backGroundVolumeLabel;
+	cocos2d::Label* _effectsVolumeLabel;
+	cocos2d::Label* dificulty_Label;
+
+	cocos2d::ui::Button* us;
+	cocos2d::ui::Button* mex;
+
+	void actionButtonBack();
+	void actionBackGroundVolumeSlider();
+	void actionEffectsVolumeSlider();
+
+	void langmex();
+	void langus();
+
+public:
+	virtual bool init();
+	static cocos2d::Scene* createScene();
+	CREATE_FUNC(OptionsMenu);
+	
 };
 
