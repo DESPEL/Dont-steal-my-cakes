a57af21326e191b15a7f010a4b39e012a527f242
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2019-11-29 01:40:09 -0800

    Update MiniGameScene.cpp

--- JuegoClase/Classes/Scenes/MiniGameScene.cpp
+++ JuegoClase/Classes/Scenes/MiniGameScene.cpp
@@ -166,7 +166,6 @@ void MiniGameScene::update(float delta) {
 	_player->update(delta);
 
 	if (Tiempo < 0) {
-		cocos2d::experimental::AudioEngine::stop(musictag);
 		GameManager::getInstance()->saveMiniGameScore(_puntos);
 		cocos2d::experimental::AudioEngine::stop(musictag);
 		this->wrapper->death();
