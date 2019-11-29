bb0ef8db5681c0095fe54caebe77cb10ba7a7e68
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2019-11-29 08:51:34 -0800

    D:

--- JuegoClase/Classes/Scenes/TopScene.cpp
+++ JuegoClase/Classes/Scenes/TopScene.cpp
@@ -1,6 +1,11 @@
 #include "TopScene.h"
 #include "GameManager.h"
 #include "ui/CocosGUI.h"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
 
 USING_NS_CC;
 using namespace ui;
@@ -18,23 +23,10 @@ bool TopMenu::init() {
 		return false;
 	}
 
-	//std::vector<std::function<void>*> functions;
-	//cocos2d::Vector buttons;
 
-	//initFunctions(functions);
-	//insertamos los botones a la escena
 	initButtons();
 
-	//habilitamos todos los niveles que el jugador puede realizar actualmente
-	/*for (int i = 0; i < GameManager::NUM_LEVELS; i++) {
-		if (i > GameManager::getInstance()->getNextLevel()) break;
-		//habilitamos el boton
-		buttons.at(i)->setEnabled(true);
-		//cambiamos la imagen a habilitado
-		buttons.at(i)->setBright(true);
-	}*/
-
-	//boton de ir hacia atras
+	
 	auto backBt = Button::create("menus/Botones/back0.png", "menus/Botones/back1.png", "menus/Botones/back1.png", Widget::TextureResType::LOCAL);
 	backBt->setAnchorPoint(Point(0, 0.5));
 	backBt->setScale(2);
@@ -52,10 +44,43 @@ void TopMenu::initButtons() {
 	High->setPosition(_visibleSize.width/2, 290);
 	addChild(High, 5);
 
-
+	Scores = Label::createWithTTF("LA\nLA\nLA\nLA\nLA", "fonts/arial.ttf", 30);
+	MostarPuntaje();
+	Scores->setPosition(_visibleSize.width / 2, _visibleSize.height / 2);
+	addChild(Scores);
 	
 }
 
 void TopMenu::actionButtonBack() {
 	Director::getInstance()->pushScene(TransitionFadeBL::create(1, MainMenu::createScene()));
+}
+using namespace std;
+void TopMenu::MostarPuntaje() {
+
+	string LOL;
+	string txt="";
+	int num;
+
+	ifstream estado("puntajes.txt");
+	
+	while (!estado.eof()) {
+		getline(estado, LOL);
+		Jugadores.push_back(LOL);
+		std::istringstream iss(LOL);
+		iss >> num;
+		puntos.push_back(num);
+	}
+	sort(puntos.begin(), puntos.end(), std::greater<int>());
+	estado.close();
+	for (int i = 0; i < Jugadores.size(); i++) {
+		if (i >= 5) {
+			break;
+		}
+		txt += to_string(i+1)+") " + to_string(puntos[i]) + "\n";
+		
+	}
+	Scores->setString(txt);
+	
+	
+	Jugadores.clear();
 }
